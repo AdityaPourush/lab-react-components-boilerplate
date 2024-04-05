4dcce044ba5454f84acdcf964a8638996567765e
AdityaPourush <adityapourush11@outlook.com>
2024-04-05 12:34:06 +0530

    done

---
+++ src/components/GallaryMain.jsx
@@ -0,0 +1,17 @@
+function GallaryMain(props) {
+    return(
+        <div>
+        {
+          props.imageData.map(image=>{
+            return(
+              <div key={image.id} className="column">
+                <img src={image.img} alt=""></img>
+              </div>
+            )
+          })
+        }
+      </div>
+    )
+}
+
+export default GallaryMain;
